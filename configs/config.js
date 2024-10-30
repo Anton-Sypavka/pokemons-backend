7393b1dfc2e6833f3dc187a6e0670be88970501a
anton.sypavka <anton.sypavka@gmail.com>
2024-10-30 20:21:52 +0200

    Changed PORT.

--- configs/config.js
+++ configs/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  PORT: process.env.PORT || 5001,
+  PORT: process.env.PORT || 5000,
   URL: process.env.API_URL || 'http://localhost:5001',
   DATABASE: process.env.DB_CONNECTION_URL || 'mongodb://localhost:27017/PokemonsDB'
 };
